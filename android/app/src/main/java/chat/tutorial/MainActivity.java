8499f01df19f26a9661549734adb1e6935cffbd6
jakkumame <nkmrysyk0914@gmail.com>
2023-03-24 10:39:04 +0900

    intergration cpacitor

---
+++ android/app/src/main/java/chat/tutorial/MainActivity.java
@@ -0,0 +1,5 @@
+package chat.tutorial;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'chat.tutorial',
+  appName: 'chat-tutorial',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
